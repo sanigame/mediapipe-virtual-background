f9b1036e5263117833d455b6920b18aae6ade1fd
Supasan Suklim <supasan.game@gmail.com>
2023-03-17 15:30:47 +0700

    Add face filter 3d

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Background from "./components/Background";
 import FaceFilter from "./components/FaceFilter";
 import FaceFilterDrawing from "./components/FaceFilterDrawing";
 import ScreenBackground from "./components/ScreenBackground";
+import FaceFilter3d from "./components/FaceFilter3d";
 
 
 const App = () => {
@@ -16,12 +17,14 @@ const App = () => {
       <button onClick={() => setBgComponent('ScreenBackground')}>ScreenBackground</button>
       <button onClick={() => setBgComponent('FaceFilter')}>FaceFilter</button>
       <button onClick={() => setBgComponent('FaceFilterDrawing')}>FaceFilterDrawing</button> 
+      <button onClick={() => setBgComponent('FaceFilter3d')}>FaceFilter3d</button> 
       <br />
       <br />
       {bgComponent === 'Background' ? <Background /> : null}
       {bgComponent === 'FaceFilter' ? <FaceFilter /> : null}
       {bgComponent === 'FaceFilterDrawing' ? <FaceFilterDrawing /> : null}
       {bgComponent === 'ScreenBackground' ? <ScreenBackground /> : null}
+      {bgComponent === 'FaceFilter3d' ? <FaceFilter3d /> : null}
     </>
   );
 }
