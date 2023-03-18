212774a8613c706089c3ded66b0b44aeef22a747
Supasan Suklim <supasan.game@gmail.com>
2023-03-18 16:00:14 +0700

    Set upload filter

--- src/components/FaceFilterAr.js
+++ src/components/FaceFilterAr.js
@@ -1,9 +1,22 @@
-import React, { useEffect, useRef } from "react";
-import {MindARThree} from 'mind-ar/dist/mindar-face-three.prod.js';
-import * as THREE from 'three';
+import React, { useEffect, useRef, useState } from "react";
+import { MindARThree } from "mind-ar/dist/mindar-face-three.prod.js";
+import * as THREE from "three";
 
 const FaceFilterAr = () => {
   const containerRef = useRef(null);
+  const [imageURL, setImageURL] = useState(
+    "/filter/canonical_face_model_uv_visualization.png"
+  );
+
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImageURL(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   useEffect(() => {
     const mindarThree = new MindARThree({
@@ -14,9 +27,7 @@ const FaceFilterAr = () => {
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
     const faceMesh = mindarThree.addFaceMesh();
-    const texture = new THREE.TextureLoader().load(
-      "/filter/face-mask.png"
-    );
+    const texture = new THREE.TextureLoader().load(imageURL);
     faceMesh.material.map = texture;
     faceMesh.material.transparent = true;
     faceMesh.material.needsUpdate = true;
@@ -27,14 +38,37 @@ const FaceFilterAr = () => {
       renderer.render(scene, camera);
     });
 
-    return () => {}
+    return () => {
+      renderer.setAnimationLoop(null);
+      mindarThree.stop();
+    };
   }, []);
 
   return (
     <>
-      <p>upload</p>
+      <div>
+        <label htmlFor="contained-button-file" className="file-upload">
+          <input
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+            onChange={imageHandler}
+          />
+          Choose Background
+        </label>
+        <div className="backgrounds" style={{ display: "none" }}>
+          <img
+            id="vbackground"
+            src={imageURL}
+            alt="The Screan"
+            className="background"
+          />
+        </div>
+      </div>
+
       <div className="container">
-        <div style={{width: "100%", height: "100%"}} ref={containerRef}></div>
+        <div style={{ width: "100%", height: "100%" }} ref={containerRef}></div>
       </div>
     </>
   );
