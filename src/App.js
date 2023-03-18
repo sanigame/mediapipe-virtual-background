7ddaa84b83427ec8c6c964fef5249c52a7c3a178
Supasan Suklim <supasan.game@gmail.com>
2023-03-18 14:47:51 +0700

    Add face filter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Background from "./components/Background";
 import FaceFilter from "./components/FaceFilter";
 import FaceFilterDrawing from "./components/FaceFilterDrawing";
 import ScreenBackground from "./components/ScreenBackground";
-import FaceFilter3d from "./components/FaceFilter3d";
+// import FaceFilter3d from "./components/FaceFilter3d";
 import FaceFilterAr from "./components/FaceFilterAr";
 
 
@@ -18,7 +18,7 @@ const App = () => {
       <button onClick={() => setBgComponent('ScreenBackground')}>ScreenBackground</button>
       <button onClick={() => setBgComponent('FaceFilter')}>FaceFilter</button>
       <button onClick={() => setBgComponent('FaceFilterDrawing')}>FaceFilterDrawing</button> 
-      <button onClick={() => setBgComponent('FaceFilter3d')}>FaceFilter3d</button> 
+      {/* <button onClick={() => setBgComponent('FaceFilter3d')}>FaceFilter3d</button>  */}
       <button onClick={() => setBgComponent('FaceFilterAr')}>FaceFilterAr</button> 
       <br />
       <br />
@@ -26,7 +26,7 @@ const App = () => {
       {bgComponent === 'FaceFilter' ? <FaceFilter /> : null}
       {bgComponent === 'FaceFilterDrawing' ? <FaceFilterDrawing /> : null}
       {bgComponent === 'ScreenBackground' ? <ScreenBackground /> : null}
-      {bgComponent === 'FaceFilter3d' ? <FaceFilter3d /> : null}
+      {/* {bgComponent === 'FaceFilter3d' ? <FaceFilter3d /> : null} */}
       {bgComponent === 'FaceFilterAr' ? <FaceFilterAr /> : null}
     </>
   );
--- src/components/FaceFilterAr.js
+++ src/components/FaceFilterAr.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import {MindARThree} from 'mind-ar/dist/mindar-face-three.prod.js';
+import * as THREE from 'three';
 
 const FaceFilterAr = () => {
-  return <div>FaceFilterAr</div>;
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const mindarThree = new MindARThree({
+      container: containerRef.current,
+    });
+
+    const { renderer, scene, camera } = mindarThree;
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+    const faceMesh = mindarThree.addFaceMesh();
+    const texture = new THREE.TextureLoader().load(
+      "https://raw.githubusercontent.com/google/mediapipe/master/mediapipe/modules/face_geometry/data/canonical_face_model_uv_visualization.png"
+    );
+    faceMesh.material.map = texture;
+    faceMesh.material.transparent = true;
+    faceMesh.material.needsUpdate = true;
+    scene.add(faceMesh);
+
+    mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+
+    return () => {}
+  }, []);
+
+  return <div style={{width: "100%", height: "100%"}} ref={containerRef}></div>;
 };
 
 export default FaceFilterAr;
